37278992d2b0b7aed0ff0c448e0c40397849468b
priyal-codes <priyaldotcodes@gmail.com>
2026-06-23 16:05:54 +0530

    initial commit: setup backend and configure environmental variables

---
+++ backend/src/app.js
@@ -0,0 +1,32 @@
+import express from "express";
+import { createServer } from "node:http";
+
+import { Server } from "socket.io";
+
+import mongoose from "mongoose";
+import { connectToSocket } from "./controllers/socketManager.js"; 
+
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+app.set("port", (process.env.PORT || 8000))
+app.use(cors());
+app.use(express.json({limit: "40kb"}));
+app.use(express.urlencoded({limit: "40kb", extended: true}));
+
+const start = async () => {
+    const connectionDb = await mongoose.connect(process.env.MONGO_URL)
+    console.log(`MONGO Connected DB Host: ${connectionDb.connection.host} `)
+    server.listen(app.get("port"), () => {
+        console.log("LISTENING ON PORT 8000")
+    });
+
+}
+
+start();
