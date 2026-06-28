873c884c5e484c180c797fb9c56e30f9458db867
priyal-codes <priyaldotcodes@gmail.com>
2026-06-28 22:22:43 +0530

    added videomeet skeleton and route

---
+++ frontend/src/pages/VideoMeet.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+
+import "../styles/videoComponent.css";
+
+const server_url = "http://localhost:8000";
+
+var connections = {};
+
+const peerConfigConnections = {
+    "iceServers": [
+        { "urls": "stun:stun.l.google.com:19302"}
+    ]
+}
+
+export default function VideoMeetComponent() {
+
+    var socketRef = useRef();
+    let socketIdRef = useRef();
+
+    let localVideoRef = useRef();
+
+    let [videoAvailable, setVideoAvailable] = useState(true);
+
+    let [audioAvailable, setAudioAvailable] = useState(true);
+
+    let [video, setVideo] = useState();
+
+    let [audio, setAudio] = useState();
+
+    let [screen, setScreen] = useState();
+
+    let [showModal, setModal] = useState();
+
+    let [screenAvailable, setScreenAvailable] = useState();
+
+    let [messages, setMessages] = useState("");
+
+    let [newMessages, setNewMessages] = useState(0);
+
+    let [username, setUsername] = useState("");
+
+    const videoRef = useRef([])
+
+    let [videos, setVideos] = useState([])
+
+
+
+    return (
+        <div>
+            {askForUsername === true ?
+            <div>
+
+            </div> : <></> 
+
+            }
+        </div>
+    )
+} 
