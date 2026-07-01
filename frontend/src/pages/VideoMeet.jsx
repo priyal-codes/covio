9e453ce6376ec74e13e83cb36eea72c305c2743f
priyal-codes <priyaldotcodes@gmail.com>
2026-07-01 22:41:39 +0530

    minor changes

--- frontend/src/pages/VideoMeet.jsx
+++ frontend/src/pages/VideoMeet.jsx
@@ -118,7 +118,22 @@ export default function VideoMeetComponent() {
 
     //TODO
     let gotMessageFromServer = (fromId, messages) => {
-         
+         var signal = JSON.parse(message)
+
+         if(fromId == socketIdRef.current) {
+            if(signal.sdp) {
+                connections[fromId].setRemoteDescription(new RTCSessionDescription(signal.sdp)).then(() => {
+                    if(signal.sdp.type == "offer" ) {
+
+                        connections[fromId].createAnswer().then((description)=> {
+                           connections[fromId].setLocalDescription(description).then(()=>{
+                            socketIdRef.current.emit("signal", fromId, JSON.stringify({"sdp": connections[fromId].localDescription}))
+                           }).catch(e => console.log(e)) 
+                        })
+                    }
+                })
+            }
+         }
     }
 
     //TODO addMessage
